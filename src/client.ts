b00fbce474d0c5b0b5abda10a149b79bd9667ef4
Green <shren0812@gmail.com>
2022-06-03 10:45:29 +0900

    잡다한 코드 수정

--- src/client.ts
+++ src/client.ts
@@ -5,20 +5,21 @@ const client = new ApolloClient({
   cache: new InMemoryCache()
 })
 
+
 // 이렇게 작성하는 방식은 hooks이 나오기 이전의 방식이다. (구림)
-client.query({
-    query: gql`
-        {
-            findAll {
-                id
-                firstName
-                lastName
-                fullName
-                address
-            }
-        }
-    `
-}).then(data => console.log(data))
+// client.query({
+//     query: gql`
+//         {
+//             findAll {
+//                 id
+//                 firstName
+//                 lastName
+//                 fullName
+//                 address
+//             }
+//         }
+//     `
+// }).then(data => console.log(data))
 
 export default client;
 // 보통은 apollo client가 가지고 있는 hooks을 이용한다.(useQuery, useMutation 등...)
